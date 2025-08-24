ac69a4f6f559bc423a36e8584b068a12d291342e
pornnewbee <65003745+pornnewbee@users.noreply.github.com>
2025-08-24 18:05:56 +0800

    Update httprequest.py

--- scripts/httprequest.py
+++ scripts/httprequest.py
@@ -2,9 +2,10 @@ import requests
 
 url = "https://ipinfo.io/ip"
 
-for i in range(1, 101):
+for i in range(1, 11):
     try:
         response = requests.get(url, timeout=5)
         print(f"{i}: 状态码 {response.status_code}")
     except requests.RequestException as e:
         print(f"{i}: 请求失败 {e}")
+
